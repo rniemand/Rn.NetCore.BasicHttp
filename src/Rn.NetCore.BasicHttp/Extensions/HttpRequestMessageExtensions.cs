3a0b6aa0cbde919931716fb018384a71256ba1c8
Richard Niemand <niemand.richard@gmail.com>
2022-05-10 13:54:32 -0600

    Cleanup

--- src/Rn.NetCore.BasicHttp/Extensions/HttpRequestMessageExtensions.cs
+++ src/Rn.NetCore.BasicHttp/Extensions/HttpRequestMessageExtensions.cs
@@ -9,15 +9,20 @@ public static class HttpRequestMessageExtensions
 
   public static TimeSpan? GetTimeout(this HttpRequestMessage request)
   {
-    if (request == null) throw new ArgumentNullException(nameof(request));
+    if (request == null)
+      throw new ArgumentNullException(nameof(request));
+
     if (request.Properties.TryGetValue(TimeoutPropertyKey, out var value) && value is TimeSpan timeout)
       return timeout;
+
     return null;
   }
 
   public static void SetTimeout(this HttpRequestMessage request, TimeSpan? timeout)
   {
-    if (request == null) throw new ArgumentNullException(nameof(request));
+    if (request == null)
+      throw new ArgumentNullException(nameof(request));
+
     request.Properties[TimeoutPropertyKey] = timeout;
   }
 }
